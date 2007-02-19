cf84a71d7d35c704609d0f92502e183471bdecc7
ikeji <ikeji@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2007-02-19 15:02:09 +0000

    custom_feed handling date and link thx to moonrock
    git-svn-id: http://dev.ikejima.org/src/svn/pragger/trunk@310 f8368ae4-1e99-dc11-9799-0018f35ab5fa

--- plugin/custom_feed.rb
+++ plugin/custom_feed.rb
@@ -6,10 +6,10 @@ def custom_feed(config,data)
   items = []
   data.each do |input|
     body = ""
-    pp(config['capture'])
     if(config['capture'])
-      input =~ Regexp.new(config['capture'],Regexp::MULTILINE)
-      body = $1
+      if input =~ Regexp.new(config['capture'],Regexp::MULTILINE)
+        body = $1
+      end
     else
       body = input
     end
@@ -26,16 +26,47 @@ def custom_feed(config,data)
   if(config['title'])
     title = Regexp.new(config['title'])
     items.each do|i|
-      i =~ title
-      mytitle = $1
-      i.instance_eval do 
-        @title = mytitle
-        def title
-          @title
+      if i =~ title
+        mytitle = $1
+        i.instance_eval do 
+          @title = mytitle
+          def title
+            @title
+          end
         end
       end
     end
   end
 
+  if(config['date'])
+    date = Regexp.new(config['date'])
+    items.each do|i|
+      if i =~ date
+        mydate = Time.parse($1)
+        i.instance_eval do
+          @date = mydate
+          def date
+            @date
+          end
+        end
+      end
+    end
+  end
+
+  if config['link'] and config['url']
+    link = Regexp.new(config['link'])
+    items.each do|i|
+      if i =~ link
+        mylink = config['url'] ? (config['url'] + '#' + $1) : $1 
+        i.instance_eval do
+          @link = mylink
+          def link
+            @link
+          end
+        end
+      end
+    end
+  end
+  
   return items
 end
