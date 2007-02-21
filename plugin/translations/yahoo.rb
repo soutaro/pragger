55ee274a1d8f758c2ee4da32ff55c4bdc1b26361
soutaro <soutaro@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2007-02-21 18:45:05 +0000

    translation by http://honyaku.yahoo.co.jp/transtext
    git-svn-id: http://dev.ikejima.org/src/svn/pragger/trunk@324 f8368ae4-1e99-dc11-9799-0018f35ab5fa

---
+++ plugin/translations/yahoo.rb
@@ -0,0 +1,32 @@
+begin
+  require 'rubygems'
+rescue LoadError
+end
+require 'mechanize'
+
+def yahoo(config, data)
+  config = (config || { "translation" => "en=>ja" })
+
+  trans = case config["translation"]
+          when "en=>ja": "EJ"
+          when "kr=>ja": "KJ"
+          when "cn=>ja": "CJ"
+          when "ja=>en": "JE"
+          when "ja=>kr": "JK"
+          when "ja=>cn": "JC"
+          else
+            return data
+          end
+  
+  data.collect {|d|
+    agent = WWW::Mechanize.new
+    start = agent.get("http://honyaku.yahoo.co.jp/")
+    form = start.forms.last
+    
+    form.radiobuttons.each {|radio| radio.checked = (radio.value =~ /#{trans}/) ? true : false }
+    form.fields.name("text").first.value = d
+    
+    result = agent.submit(form)
+    result.forms.name("textFormEntry").fields.name("trn_text").value
+  }
+end
