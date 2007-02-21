a8895064f272269a25ee4f81394793e684dd307e
soutaro <soutaro@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2007-02-21 19:47:03 +0000

    apply pragger to text nodes in HTML
    git-svn-id: http://dev.ikejima.org/src/svn/pragger/trunk@327 f8368ae4-1e99-dc11-9799-0018f35ab5fa

---
+++ plugin/apply_text_html.rb
@@ -0,0 +1,25 @@
+begin
+  require 'rubygems'
+rescue LoadError
+end
+require 'hpricot'
+
+def apply_text_html(config, data)
+  data.collect {|d|
+    doc = Hpricot(d.to_s.toutf8)
+    texts = []
+    doc.traverse_text {|text|
+      texts << text.to_s
+    }
+    
+    data2 = eval_pragger(config, texts)
+    
+    result_html = d.to_s.toutf8
+    
+    Hash[*texts.zip(data2).flatten].each {|k,v|
+      result_html.sub!(k,v)
+    }
+    
+    result_html
+  }
+end
