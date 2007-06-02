f437bb1a32ac847557a3c288ccc576eb3512f823
takatoh <takatoh@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2007-06-02 11:41:54 +0000

    Update Publish::hatena_graph plugin.
    git-svn-id: http://dev.ikejima.org/src/svn/pragger/trunk@365 f8368ae4-1e99-dc11-9799-0018f35ab5fa

--- plugin/Publish/hatena_graph.rb
+++ plugin/Publish/hatena_graph.rb
@@ -1,63 +1,36 @@
 ## Post data to Hatena Graph -- takatoh
 ## 
-## see http://d.hatena.ne.jp/takatoh/20070503/hatena_graph
+## hatenaapigraph 0.1.2 is required.
+## 
+## see http://d.hatena.ne.jp/takatoh/20070531/hatena_graph
 ##
 ## - module: Publish::hatena_graph
 ##   config:
 ##     user_id: your hatena user id
 ##     password: your password
 ##     graph_name: the name of graph
+##     proxy_host: proxy host name   (optional)
+##     proxy_port: proxy port        (optional)
+##     proxy_user: proxy user        (optional)
+##     proxy_pass: proxy password    (optional)
 
 begin
   require 'rubygems'
+  gem 'hatenaapigraph', '>=0.1.2'
 rescue LoadError
 end
 require 'hatena/api/graph'
 
 
-# bug fix and extention for hatenaapigraph 0.1.0
-#
-module ::Hatena
-  module API
-    class Graph
-      def post(graphname, date, value)
-        value = value.to_f
-        date = date.strftime DATE_FORMAT
-        headers = {
-          'Access' => 'application/x.atom+xml, application/xml, text /xml, */*',
-          'X-WSSE' => wsse(@username, @password),
-        }
-        params = {
-          :graphname => graphname,
-          :date => date,
-          :value => value,
-        }
-        res = http_post GRAPH_API_URI, params, headers
-        raise GraphError.new("request not successed: #{res}") if res .code != '201'
-        res
-      end
-
-      private 
-      def http_post(url, params, headers)
-        req = ::Net::HTTP::Post.new(url.path, headers)
-        req.form_data = params
-        req.basic_auth url.user, url.password if url.user
-        proxy_host = nil
-        proxy_port = nil
-        if proxy = ENV['http_proxy']
-          proxy = URI.parse(proxy)
-          proxy_host = proxy.host
-          proxy_port = proxy.port
-        end
-        ::Net::HTTP.new(url.host, url.port, proxy_host, proxy_port). start {|http| http.request(req) }
-      end
-    end
-  end
-end
-
-
 def hatena_graph(config, data)
   value = data.first.to_f
-  graph = Hatena::API::Graph.new(config['user_id'], config['password '])
+  graph = Hatena::API::Graph.new(config['user_id'], config['password'])
+  if config['proxy_host']
+    proxy_host = config['proxy_host']
+    proxy_port = config['proxy_port']
+    proxy_user = config['proxy_user']
+    proxy_pass = config['proxy_pass']
+    graph.proxy = ::Net::HTTP.Proxy(proxy_host, proxy_port, proxy_user, proxy_pass)
+  end
   graph.post(config['graph_name'], Time.now, value)
 end
