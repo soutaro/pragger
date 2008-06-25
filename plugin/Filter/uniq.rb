b25f829c150a48be8c385d842824edfc73fd7b0d
ikeji <ikeji@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2008-06-25 15:36:15 +0000

    add uniq plugins
    git-svn-id: http://dev.ikejima.org/src/svn/pragger/trunk@386 f8368ae4-1e99-dc11-9799-0018f35ab5fa

---
+++ plugin/Filter/uniq.rb
@@ -0,0 +1,15 @@
+## uniq -- IKeJI
+## 
+## it looks like uniq command in unix
+
+def uniq(config,data)
+  result = {}
+  data.reverse.each_with_index do|i,ind|
+    result[ (i.link rescue i) ] = [ind,i]
+  end
+  data = []
+  result.values.sort{|a,b| a[0]<=>b[0]}.each do |a|
+    data << a[1]
+  end
+  return data
+end
