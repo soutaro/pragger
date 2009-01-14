9f37b94ed5909604a6882ac1f441853dd7a6b3fa
ikeji <ikeji@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2009-01-14 14:49:22 +0000

    Add plugin Feed::mixi::get_diary_list.
    git-svn-id: http://dev.ikejima.org/src/svn/pragger/trunk@395 f8368ae4-1e99-dc11-9799-0018f35ab5fa

---
+++ plugin/Feed/mixi/get_diary_list.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'mechanize'
+
+def get_diary_list(config,data)
+  username = $reg['mixi']['username']
+  password = $reg['mixi']['password']
+
+  client = WWW::Mechanize.new
+  page = client.get('http://mixi.jp/')
+  sleep 5
+  form = page.forms[0]
+  form.fields.find {|f| f.name == 'email'}.value = username
+  form.fields.find {|f| f.name == 'password'}.value = password
+  form.fields.find {|f| f.name == 'next_url'}.value = '/home.pl'
+  page = client.submit(form, form.buttons.first)
+  sleep 5
+
+  if /url=([^"])"/ =~ page.body
+    link = 'http://mixi.jp' + $1.to_s
+    client.get(link)
+    sleep 5
+  end
+
+  data = []
+  diarylist = client.get("http://mixi.jp/list_diary.pl?id=" + config['id'].to_s)
+  sleep 5
+  diarylist.links.each do |link|
+    diaryurl = link.href # => 'view_diary.pl?id=12345&owner_id=2345'
+    if /view_diary.pl\?id\=/ =~ diaryurl then
+      data << 'http://mixi.jp/' + diaryurl
+    end
+  end
+  return data.uniq
+end
