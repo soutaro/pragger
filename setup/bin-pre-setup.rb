381babf93a30e11658addea70ef68e37439f2558
soutaro <soutaro@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2009-03-26 15:16:54 +0000

    pragger.rb after installation is renamed to pragger
    git-svn-id: http://dev.ikejima.org/src/svn/pragger/trunk@399 f8368ae4-1e99-dc11-9799-0018f35ab5fa

--- setup/bin-pre-setup.rb
+++ setup/bin-pre-setup.rb
@@ -4,6 +4,7 @@ def update_plugin_dir(input)
   input.gsub('Pathname(__FILE__).parent', "Pathname('#{config("datadir")}/pragger')")
 end
 
-pragger = Pathname("pragger.rb")
+pragger = Pathname("pragger")
 content = pragger.read
 pragger.open('w'){|io| io.write(update_plugin_dir(content)) }
+system "chmod +x pragger"
