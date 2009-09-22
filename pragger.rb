1ac0fbfb9caf931bc2db9dd1158bd5e00a1b44db
soutaro <soutaro@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2009-09-22 13:49:08 +0000

    for case if config.yaml is empty
    git-svn-id: http://dev.ikejima.org/src/svn/pragger/trunk@415 f8368ae4-1e99-dc11-9799-0018f35ab5fa

--- pragger.rb
+++ pragger.rb
@@ -10,7 +10,7 @@ $plugins = {}
 class Plugin
   attr_reader :source
   def initialize(file)
-    instance_eval( @source = File.read(file).toutf8, file , 1)
+    instance_eval( @source = File.read(file).toutf8, file.to_s , 1)
   end
   def self.load_plugins(folder = (Pathname(__FILE__).parent + "plugin"))
     Pathname.glob(folder.to_s + "**/*.rb").sort.each do |file|
@@ -23,7 +23,7 @@ class Plugin
 end
 
 def eval_pragger(command_array,data)
-  command_array.inject(data) do |data, command|
+  (command_array || []).inject(data) do |data, command|
     $plugins[command["module"]].send(command["module"].sub(/.*::/,""), command["config"] || {}, data.clone)
   end
 end
@@ -36,6 +36,7 @@ opt.on("-p", "--plugindir PLUGINDIR") {|v| Plugin.load_plugins(Pathname(v)) }
 opt.on("-u", "--pluginusage PLUGINNAME") {|v| $plugins[v].source.gsub(/^## ?(.*)/){ puts $1 }; exit }
 opt.on("-l", "--listplugin") { $plugins.keys.sort.each{|k| puts k }; exit }
 opt.on("-w", "--where") { puts(Pathname(__FILE__).parent + "plugin"); exit }
+opt.on("-v", "--verbose") { $verbose = true }
 opt.parse!
 
 eval_pragger(YAML.load(File.read(configFile).toutf8.gsub(/base64::([\w+\/]+=*)/){ Base64.decode64($1) }),[])
